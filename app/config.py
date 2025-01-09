bcbc10b3357d13541ca9fe7728c2fca35982e2f1
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-09 22:37:05 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/mrp'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    # Database URI for deployment on Aiven
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'mysql+pymysql://avnadmin:AVNS_64D7XhVDVS5mweyqAHs@mysql-615390b-matrium-24.h.aivencloud.com:21017/mrp'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
