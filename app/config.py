c8d0c2d5fa03b82fa31360e9e5bf34628073fbd2
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-09 20:07:43 +0100

    Delete app directory

--- app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://avnadmin:AVNS_64D7XhVDVS5mweyqAHs@mysql-615390b-matrium-24.h.aivencloud.com/mrp'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
--- app/index.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-@app.route('/')
-def health_check():
-    return "Flask app is running on Vercel!", 200
