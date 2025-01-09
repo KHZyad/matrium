e645d1e578479d3fd796288d1273495c44e36c99
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-09 20:07:56 +0100

    Delete mrpFlask directory

--- mrpFlask/app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/mrp'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
--- mrpFlask/app/routes/defect_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-defect_bp = Blueprint('defect', __name__)
-
-# Placeholder route
-@defect_bp.route('/test', methods=['GET'])
-def test_defect():
-    return "Defect routes are working!"
